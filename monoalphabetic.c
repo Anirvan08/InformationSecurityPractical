891f1592b984cfc28d534d6fdea22b3548e8576f
Anirvan Goswami <53483837+Anirvan08@users.noreply.github.com>
2022-05-03 16:16:39 +0530

    Update monoalphabetic.c

--- monoalphabetic.c
+++ monoalphabetic.c
@@ -1,110 +1,77 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-using namespace std;
-int main()
-{
-    int i, j, k, n;
-    cout << "Enter the message" << endl;
-    string s, origin;
-    getline(cin, origin);
-    cout << "Enter the key" << endl;
-    string key;
-    cin >> key;
-    for (i = 0; i < origin.size(); i++)
-    {
-        if (origin[i] != ' ')
-            s += origin[i];
-    }
-    vector<vector<char>> a(5, vector<char>(5, ' '));
-    n = 5;
-    map<char, int> mp;
-    k = 0;
-    int pi, pj;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            while (mp[key[k]] > 0 && k < key.size())
-            {
-                k++;
-            }
-            if (k < key.size())
-            {
-                a[i][j] = key[k];
-                mp[key[k]]++;
-                pi = i;
-                pj = j;
-            }
-            if (k == key.size())
-                break;
-        }
-        if (k == key.size())
-            break;
-    }
-    k = 0;
-    for (; i < n; i++)
-    {
-        for (; j < n; j++)
-        {
-            while (mp[char(k + 'a')] > 0 && k < 26)
-            {
-                k++;
-            }
-            if (char(k + 'a') == 'j')
-            {
-                j--;
-                k++;
-                continue;
-            }
-            if (k < 26)
-            {
-                a[i][j] = char(k + 'a');
-                mp[char(k + 'a')]++;
-            }
-        }
-        j = 0;
-    }
-    string ans;
-    if (s.size() % 2 == 1)
-        s += "x";
-    for (i = 0; i < s.size() - 1; i++)
-    {
-        if (s[i] == s[i + 1])
-            s[i + 1] = 'x';
-    }
-    map<char, pair<int, int>> mp2;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            mp2[a[i][j]] = make_pair(i, j);
-        }
-    }
 
-    for (i = 0; i < s.size() - 1; i += 2)
-    {
-        int y1 = mp2[s[i]].first;
-        int x1 = mp2[s[i]].second;
-        int y2 = mp2[s[i + 1]].first;
-        int x2 = mp2[s[i + 1]].second;
-        if (y1 == y2)
-        {
-            ans += a[y1][(x1 + 1) % 5];
-            ans += a[y1][(x2 + 1) % 5];
-        }
-        else if (x1 == x2)
-        {
-            ans += a[(y1 + 1) % 5][x1];
-            ans += a[(y2 + 1) % 5][x2];
-        }
-        else
-        {
-            ans += a[y1][x2];
-            ans += a[y2][x1];
-        }
-    }
-    cout << ans << '\n';
-    return 0;
+#include<stdio.h>
+
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+
+main()
+{
+
+char plain[200],encr[200],encr1[200],decr[200];
+int ch,flag[26];
+char ch1;
+char per[26];
+int i,j,size,k=0,a=0;
+char pos;
+
+printf("\nEnter the plain text");
+gets(plain);
+size=strlen(plain);
+printf("\n size= %d",size);
+ for(i=0;i<26;i++)
+  flag[i]=0;
+
+ for(i=0;i<26;i++)
+ {
+   a=rand()%26;
+  while(flag[a]==1)
+   a=rand()%26;
+  flag[a]=1;
+  per[i]=(char)(a+97);
+ }
+printf("\nPermuted array is: ");
+for(i=0;i<26;i++)
+{
+printf("%c ",per[i]);
+}
+for(j=0;j<size;j++)
+{
+ch=plain[j];
+ch=ch-97;
+ch1=per[ch];
+
+encr1[j]=ch1;
+}
+
+
+printf("\nthe encrypted string is ");
+for(i=0;i<size;i++)
+{
+printf("%c",encr1[i]);
+}
+
+
+//decryption
+ i=0;
+ while(i<size)
+ {
+  //a=ct[i];
+  //a=a-97;
+  //pt[i]=key[a];
+  a=0;
+  while(a!=26)
+  {
+   if(per[a]==encr1[i])
+   {
+    decr[i]=a+97;
+    break;
+   }
+   a++;
+  }
+  i++;
+ }
+ decr[i]='\0';
+ printf("\nDecrypted string: %s\n",decr);
+ getch();
 }
